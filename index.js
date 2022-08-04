a24f4200dd5051f74499d1de0126f1e8250ed112
Babykala <babykala3@gmail.com>
2022-08-04 19:15:38 +0530

    first commit

--- index.js
+++ index.js
@@ -24,23 +24,6 @@ app.use('/get',async(req,res,next)=>{
     }
 })
 
-app.use('/get',async(req,res,next)=>{
-    try{
-        const price=await priceModel.find();
-        return res.send(price);
-        
-    }catch(err){
-        console.log(err);
-    }
-})
-app.use('/create',async(req,res,next)=>{
-    const city=new priceModel({...req.body})
-        try{
-            var response=await city.save();
-            res.send(response);
-        }
-        catch(e){console.log(e)}
-})
 const port=process.env.PORT||8080
 
 app.listen(port,()=> {
