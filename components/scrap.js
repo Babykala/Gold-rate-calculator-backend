3f8b7f5f1206f81f3d4e459b4bef5751653e478e
Babykala <babykala3@gmail.com>
2022-08-05 13:32:25 +0530

    first commit

--- components/scrap.js
+++ components/scrap.js
@@ -28,8 +28,9 @@ module.exports={
                 // })
                 var myquery = { city:cities[i] };
                 var newvalues = {$set: {price:result[0].trim().replace(/,/g, ''),gram:result[1].trim(),dateTime:result[2]} };
-                priceList.updateMany(myquery, newvalues, function(res, err) {
-                //   console.log(res)
+                priceList.updateMany(myquery, newvalues, function(err, res) {
+                    try{console.log(res)}
+                    catch (err){throw err} 
                 })
                 
                 browser.close();
